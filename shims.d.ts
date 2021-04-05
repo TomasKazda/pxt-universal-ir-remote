c64183f582ba9852b35b6e5c3af1a29718d7bb49
Tomáš Kazda <36702826+TomasKazda@users.noreply.github.com>
2021-04-05 17:53:24 +0200

    dts

---
+++ shims.d.ts
@@ -0,0 +1,16 @@
+// Auto-generated. Do not edit.
+
+
+    /**
+     * OSOYOO_IR (Version 2.0.0)
+     *
+     * Copyright (C) 2020 Yahboom
+     * 
+     */
+    //% color=#008B00 weight=11
+    //% icon="\uf1eb"
+    //% block="IR remote"
+declare namespace IR {
+}
+
+// Auto-generated. Do not edit. Really.
